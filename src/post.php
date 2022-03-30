36267af03a390a7997af85af0b5c3da293385919
morrowi <morrowi@ya.ru>
2022-03-30 14:26:06 +0300

    update 2 30.03.2022

--- src/post.php
+++ src/post.php
@@ -1,17 +1,17 @@
 <?php
 // Allow from any origin
 if (isset($_SERVER['HTTP_ORIGIN'])) {
-  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-  header('Access-Control-Allow-Credentials: true');
-  header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
 }
 // Access-Control headers are received during OPTIONS requests
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-    header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-  exit(0);
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    exit(0);
 }
 
 $postData = file_get_contents('php://input');
@@ -21,17 +21,17 @@ $data = json_decode($postData, true);
 
 if($data['url'] == 'subscription-data'){
 
-  echo exec('curl -X POST "https://stage.wapiserv.qrm.ooo/billing/subscription-data/" -H  "accept: application/json" -H  "X-Api-Key: ViVuB0oA.Fs0pQYpduk99zxZoeZR6gVhFx8itg4yP" -H  "Content-Type: application/json" -H  "X-CSRFToken: Lfyj6g6EYPe9uXfACfEpOE2StPJtzkbwzhxKKBxMWvNB8ExiMo45L3NRoUWWscru" -d "{  \"email\": \"dev@qrm.ooo\",  \"pin\": \"11111\"}"');
+    echo exec('curl -X POST "https://stage.wapiserv.qrm.ooo/billing/subscription-data/" -H  "accept: application/json" -H  "X-Api-Key: *****" -H  "Content-Type: application/json" -H  "X-CSRFToken: *******" -d "{  \"email\": \"dev@qrm.ooo\",  \"pin\": \"11111\"}"');
 
 }elseif($data['url'] == 'qr_code'){
 
-  $parsed_prices = '';
+    $parsed_prices = '';
 //$prices = $data['summ'];
-  $prices = 1;
-  $prices = preg_replace('/\(.*\)| \(.*\)/', '', $prices);
-  $delimetr = substr($prices,-3,1);
-  $price = preg_replace('/[^\d]/', '', $prices);
-  $parsed_prices = ($delimetr == ',' || $delimetr == '.') ? $price : $price.'00';
+    $prices = 1;
+    $prices = preg_replace('/\(.*\)| \(.*\)/', '', $prices);
+    $delimetr = substr($prices,-3,1);
+    $price = preg_replace('/[^\d]/', '', $prices);
+    $parsed_prices = ($delimetr == ',' || $delimetr == '.') ? $price : $price.'00';
 
-  echo exec('curl -X POST "https://wapiserv.qrm.ooo/operations/qr-code/" -H "accept: application/json" -H "X-Api-Key: J8LUjbGJ.UBZksq5n9O443l3Nw93rbqSdDjIb0ZpZ" -H "Content-Type: application/json" -H "X-CSRFToken: XeBqlIvv91UiIZYHZWJrKBEYEpLSs8BmwZBlRQAgSUJcBVKWduFHuN55JFVUfftS" -d "{ \"sum\": '.intval($parsed_prices).', \"qr_size\": 305}"');
+    echo exec('curl -X POST "https://wapiserv.qrm.ooo/operations/qr-code/" -H "accept: application/json" -H "X-Api-Key: *****" -H "Content-Type: application/json" -H "X-CSRFToken: ********" -d "{ \"sum\": '.intval($parsed_prices).', \"qr_size\": 305}"');
 }
